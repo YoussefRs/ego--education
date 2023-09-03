ad0905b6f6058db89b45581d33ae2577bfc26c94
YoussefRs <rs.youssef24@gmail.com>
2023-09-03 16:38:02 +0100

    bbb

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import Intro from './pages/intro'
 import Homepage from './pages/HomePage'
-import Course from './components/HomePageComponents/Courses/Course'
 gsap.registerPlugin(ScrollTrigger)
 
 function App() {
